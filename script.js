4a5e49b7cc57fd14f12a7e7dea62795df1c521e0
Samruddhi-sathe05 <samruddhisathe.538@gmail.com>
2026-02-03 22:01:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
+const sections = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    const pos = section.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      section.classList.add('show');
+    }
   });
 });
